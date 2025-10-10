b35759e60ac76240824266cf7ddc69afa7f41272
Chan-Sol Lee <coldkid1124@gmail.com>
2025-10-10 13:14:29 +0900

    251010

---
+++ 3. 함수/3-4-1.js
@@ -0,0 +1,13 @@
+// 함수 정의 후 export로 외부에 공개
+
+export function test(){
+    console.log("test function");
+}
+
+export function add(a, b) {
+    return a + b;
+}
+
+export function subtract(a, b) {
+    return a - b;
+}
